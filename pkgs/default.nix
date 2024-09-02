a6c6efbb9a26ab0f4a596705a3d47d564d565cb1
Krapaince <Krapaince@pm.me>
2024-09-02 21:58:01 +0200

    Inject colorbalance2 for wall-gen.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,10 +1,9 @@
-{ pkgs ? import <nixpkgs> { }, ... }: {
+{ pkgs ? import <nixpkgs> { }, ... }: rec {
   switch-audio = pkgs.callPackage ./switch-audio { };
   colorbalance2 = pkgs.callPackage ./colorbalance2 { };
 
   # Personal script
-  hyprland-ipc = pkgs.callPackage ./hyprland-ipc { };
   lock-script = pkgs.callPackage ./lock-script { };
   suspend-script = pkgs.callPackage ./suspend-script { };
-  wall-gen = pkgs.callPackage ./wall-gen { };
+  wall-gen = pkgs.callPackage ./wall-gen { inherit colorbalance2; };
 }
