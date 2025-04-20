36d813b44483ab3be33d59c23f2c46b1416585a1
krapaince <Krapaince@pm.me>
2025-04-20 12:31:58 +0200

    feat: remote disk encryption for momo

--- hosts/momo/hardware-configuration.nix
+++ hosts/momo/hardware-configuration.nix
@@ -3,9 +3,30 @@
     initrd = {
       availableKernelModules = [ "uas" ];
       systemd.tpm2.enable = false;
+      systemd.users.root.shell = "/bin/systemd-tty-ask-password-agent";
+
+      network = {
+        enable = true;
+        ssh = {
+          enable = true;
+          # Prevent conflict in known hosts
+          port = 23;
+          authorizedKeyFiles = [ ../../home/krapaince/pabu/ssh.pub ];
+          # Usage of initrd's secrets isn't supported for
+          # generic-extlinux-compatible boot loader
+          # See https://github.com/NixOS/nixpkgs/issues/247145
+          #
+          # TODO(security): some day change this
+          hostKeys = [ ./ssh_host_ed25519_key_initrd ];
+        };
+      };
     };
 
-    kernelParams = [ "console=tyAMA0,115200" "console=tty1" ];
+    kernelParams = [
+      "ip=192.168.1.57::192.168.1.1:255.255.255.0:momo::none"
+      "console=tyAMA0,115200"
+      "console=tty1"
+    ];
   };
 
   nixpkgs.hostPlatform = "aarch64-linux";
