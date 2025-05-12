42184931331cfc67b7afecad70fc3c42d00b929c
krapaince <Krapaince@pm.me>
2025-05-12 19:59:29 +0200

    fix: typo

--- hosts/common/optional/openssh.nix
+++ hosts/common/optional/openssh.nix
@@ -11,7 +11,7 @@
     };
 
     hostKeys = let
-      # Sops needs acess to the keys before the persist dirs are even mounted; so
+      # Sops needs access to the keys before the persist dirs are even mounted; so
       # just persisting the keys won't work, we must point at /persist/system
       hasOptinPersistence = config.environment.persistence ? "/persist/system";
     in [{
