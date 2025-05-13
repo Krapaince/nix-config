4c3881ec5ca8af56596c78a403bad8e4fdbf6292
krapaince <Krapaince@pm.me>
2025-05-13 21:25:07 +0200

    feat(hyprland): add smart gaps

--- home/krapaince/features/desktop/hyprland/rules.nix
+++ home/krapaince/features/desktop/hyprland/rules.nix
@@ -1,42 +1,55 @@
 {
-  wayland.windowManager.hyprland.settings.windowrulev2 = [
-    "float, title:^(?:Coping|Moving) - Dolphin"
+  wayland.windowManager.hyprland.settings = {
+    workspace = [
+      # Smart gaps (https://wiki.hyprland.org/Configuring/Workspace-Rules/#smart-gaps)
+      "w[tv1], gapsout:0, gapsin:0"
+      "f[1], gapsout:0, gapsin:0"
+    ];
+    windowrulev2 = [
+      # Smart gaps
+      "bordersize 0, floating:0, onworkspace:w[tv1]"
+      "rounding 0, floating:0, onworkspace:w[tv1]"
+      "bordersize 0, floating:0, onworkspace:f[1]"
+      "rounding 0, floating:0, onworkspace:f[1]"
 
-    "idleinhibit fullscreen, class:firefox"
+      "float, title:^(?:Coping|Moving) - Dolphin"
 
-    "idleinhibit fullscreen, title:^Picture-in-Picture$"
-    "float, title:^Picture-in-Picture$"
-    "pin, title:^Picture-in-Picture$"
-    "move 50% 1%, title:^Picture-in-Picture$"
-    "pin, class:^dragon-drop$"
+      "idleinhibit fullscreen, class:firefox"
 
-    "float, class:^Matplotlib$"
+      "idleinhibit fullscreen, title:^Picture-in-Picture$"
+      "float, title:^Picture-in-Picture$"
+      "pin, title:^Picture-in-Picture$"
+      "move 50% 1%, title:^Picture-in-Picture$"
+      "pin, class:^dragon-drop$"
 
-    "float, title:^Floating Window - Show Me The Key$"
-    "pin, title:^Floating Window - Show Me The Key$"
-    "noblur, title:^Floating Window - Show Me The Key$"
-    "opacity 1.0 override 1.0 override, title:^Floating Window - Show Me The Key$"
+      "float, class:^Matplotlib$"
 
-    "idleinhibit fullscreen, class:vlc"
+      "float, title:^Floating Window - Show Me The Key$"
+      "pin, title:^Floating Window - Show Me The Key$"
+      "noblur, title:^Floating Window - Show Me The Key$"
+      "opacity 1.0 override 1.0 override, title:^Floating Window - Show Me The Key$"
 
-    "float, title:^satty$"
-    "float, class:protonvpn-app"
+      "idleinhibit fullscreen, class:vlc"
 
-    "workspace 7 silent, class:WebCord"
-    "workspace 7 silent, class:discord"
+      "float, title:^satty$"
+      "float, class:protonvpn-app"
 
-    "workspace 8 silent, class:blueman-manager"
-    "workspace 8 silent, class:Pavucontrol"
-    "workspace 8 silent, class:TeamSpeak 3"
+      "workspace 7 silent, class:WebCord"
+      "workspace 7 silent, class:discord"
 
-    # flameshot
-    "move 0 0,class:(flameshot)"
-    "pin,class:(flameshot)"
-    "noborder,class:(flameshot)"
-    "stayfocused,class:(flameshot)"
-    "float,class:(flameshot)"
+      "workspace 8 silent, class:blueman-manager"
+      "workspace 8 silent, class:Pavucontrol"
+      "workspace 8 silent, class:TeamSpeak 3"
 
-    # avoid transparency
-    "opaque,class:(flameshot)"
-  ];
+      # flameshot
+      "move 0 0,class:(flameshot)"
+      "pin,class:(flameshot)"
+      "noborder,class:(flameshot)"
+      "stayfocused,class:(flameshot)"
+      "float,class:(flameshot)"
+
+      # avoid transparency
+      "opaque,class:(flameshot)"
+    ];
+  };
 }
