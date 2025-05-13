348cd67b6dbdd13232c47af4d99c584a7ba1fc17
krapaince <Krapaince@pm.me>
2025-05-13 21:03:11 +0200

    feat(hyprland): add an animation toggler script

--- home/krapaince/features/desktop/hyprland/bindings.nix
+++ home/krapaince/features/desktop/hyprland/bindings.nix
@@ -146,4 +146,22 @@ in {
       submap = reset
     '';
   };
+
+  home.packages = with pkgs;
+    [
+      (writeShellApplication {
+        name = "toggle-animation";
+        runtimeInputs = [ gawk hyprland ];
+        text = ''
+          HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
+
+          if [ "$HYPRGAMEMODE" = 1 ] ; then
+            hyprctl --batch "\
+              keyword animations:enabled 0;"
+          else
+            hyprctl reload
+          fi
+        '';
+      })
+    ];
 }
