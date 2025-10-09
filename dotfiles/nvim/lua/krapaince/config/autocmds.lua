c68705d5c0bdbcd54828d24e670336f15d9410b1
Krapaince <Krapaince@pm.me>
2025-10-09 21:11:24 +0200

    fix(nvim): disable line wrap for text file

--- dotfiles/nvim/lua/krapaince/config/autocmds.lua
+++ dotfiles/nvim/lua/krapaince/config/autocmds.lua
@@ -19,10 +19,9 @@ vim.api.nvim_create_autocmd('FileType', {
 
 -- wrap and check for spell in text filetypes
 vim.api.nvim_create_autocmd('FileType', {
-  group = augroup('wrap_spell'),
+  group = augroup('spell'),
   pattern = { 'gitcommit', 'markdown', 'latex', 'tex' },
   callback = function()
-    vim.opt_local.wrap = true
     vim.opt_local.spell = true
   end,
 })
