5ea83b3a98d51ee049288e0acbbfad59dd46c2d0
krapaince <krapaince@pm.me>
2025-11-04 23:41:24 +0100

    chore(nvim): update treesitter plugin

--- dotfiles/nvim/lua/krapaince/plugins/treesitter.lua
+++ dotfiles/nvim/lua/krapaince/plugins/treesitter.lua
@@ -1,59 +1,62 @@
-return {
+-- https://github.com/chrisgrieser/.config/blob/62d76218e067f966c9e6ca1b46b0bfb81ee5fc90/nvim/lua/plugin-specs/appearance/treesitter.lua
+local ensureInstalled = {
+  programmingLangs = {
+    'bash',
+    'bibtex',
+    'c',
+    'cpp',
+    'dockerfile',
+    'elixir',
+    'fish',
+    'javascript',
+    'latex',
+    'lua',
+    'nix',
+    'python',
+    'rust',
+    'typescript',
+  },
+  dataFormats = {
+    'json',
+    'json5',
+    'toml',
+    'yaml',
+  },
+  content = {
+    'css',
+    'html',
+    'markdown',
+    'mermaid',
+  },
+}
 
+return {
   {
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    branch = 'main',
     build = ':TSUpdate',
-    event = 'BufReadPost',
-    opts = {
-      ensure_installed = {
-        'bash',
-        'bibtex',
-        'c',
-        'cpp',
-        'css',
-        'dockerfile',
-        'elixir',
-        'fish',
-        'html',
-        'javascript',
-        'json',
-        'json5',
-        'latex',
-        'lua',
-        'markdown',
-        'mermaid',
-        'python',
-        'rust',
-        'toml',
-        'typescript',
-        'yaml',
-      },
-      sync_install = true,
-      context_commentstring = {
-        enable = true,
-      },
-      highlight = {
-        enable = true,
-      },
-      playground = {
-        enable = true,
-        disable = {},
-        updatetime = 25,
-        persist_queries = false,
-        keybindings = {
-          toggle_query_editor = 'o',
-          toggle_hl_groups = 'i',
-          toggle_injected_languages = 't',
-          toggle_anonymous_nodes = 'a',
-          toggle_language_display = 'I',
-          focus_language = 'f',
-          unfocus_language = 'F',
-          update = 'R',
-          goto_node = '<cr>',
-          show_help = '?',
-        },
-      },
-    },
-    main = 'nvim-treesitter.configs',
+
+    init = function()
+      vim.defer_fn(function()
+        local alreadyInstalled = require('nvim-treesitter.config').get_installed()
+        local parsersToInstall = vim
+          .iter(vim.tbl_values(ensureInstalled))
+          :flatten()
+          :filter(function(parser)
+            return not vim.tbl_contains(alreadyInstalled, parser)
+          end)
+          :totable()
+
+        require('nvim-treesitter').install(parsersToInstall)
+      end, 1000)
+      require('nvim-treesitter').update()
+
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(ctx)
+          pcall(vim.treesitter.start, ctx.buf)
+        end,
+      })
+    end,
   },
 }
